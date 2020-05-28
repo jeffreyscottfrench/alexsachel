7e6b4949527f4ab98d7ee67629150968c3d2136c
Jeffrey Scott French <jeffreysctoffrench@gmail.com>
2020-05-28 19:59:48 -0400

    Adds ability to save form input data to local storage and repopulate on page reload.

---
+++ build/assets/js/custom/formSaveLoadEntries.js
@@ -0,0 +1,19 @@
+const formSaveLoadEntries = function() {
+
+
+    let inputs = Array.from(document.querySelectorAll('input:not([type="submit"])'));
+
+    let textareas = Array.from(document.getElementsByTagName('textarea'));
+
+    let entries = inputs.concat(textareas);
+    console.log('entries: ', entries);
+
+    entries.forEach(elt => {
+      elt.value = localStorage.getItem(elt.name);
+      elt.addEventListener("change", e => {
+          localStorage.setItem(e.target.name, e.target.value);
+      });
+  });
+
+}
+formSaveLoadEntries();
