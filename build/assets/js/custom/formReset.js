a1e6c6e641c5bef9bf4a31c3d6e222737fef9a4d
Jeffrey Scott French <jeffreysctoffrench@gmail.com>
2020-05-28 19:57:14 -0400

    Updates the form reset to exclude inputs used as submit buttons. Also clears local storage of those inputs saved values.

--- build/assets/js/custom/formReset.js
+++ build/assets/js/custom/formReset.js
@@ -1,13 +1,15 @@
 const formReset = function() {
   const resetButton = document.getElementById('reset');
   resetButton.addEventListener('click', function() {
-    let inputs = Array.from(document.getElementsByTagName('input'));
+    let inputs = Array.from(document.querySelectorAll('input:not([type="submit"])'));
     inputs.forEach((input) => {
       input.value = '';
+      localStorage.removeItem(input.name);
     })
     let textareas = Array.from(document.getElementsByTagName('textarea'));
     textareas.forEach((textarea) => {
       textarea.textContent = '';
+      localStorage.removeItem(textarea.name);
     })
 
     if (document.getElementsByClassName('form__input--error')) {
