d6f3940c8e4a319070d17201febfd5611475a026
Jeffrey Scott French <jeffreysctoffrench@gmail.com>
2020-10-06 15:52:37 -0400

    Updates Dev Env browsersync to only open in Chrome.

--- gulpfile.js
+++ gulpfile.js
@@ -191,7 +191,7 @@ const browserSyncTask = function (done) {
     port: 3000,
 
     // Use a specific browser or multiple browsers ("google chrome" or multiple ["firefox", "safari technology preview"] ).
-    browser: ["google chrome", "firefox developer edition"]
+    browser: ["google chrome"]
     // browser: "google chrome"
 
   });
@@ -224,7 +224,7 @@ const browserSyncProofTask = function (done) {
     port: 8888,
 
     // Use a specific browser or multiple browsers ("google chrome" or multiple ["firefox", "safari technology preview"] ).
-    browser: ["google chrome", "firefox developer edition"]
+    browser: ["google chrome"]
 
   });
   done();
