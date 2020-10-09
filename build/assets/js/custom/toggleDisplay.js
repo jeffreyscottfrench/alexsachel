544d264b9fdc1874659ce9dbaa78e96dab3a9ed3
Jeffrey Scott French <jeffreysctoffrench@gmail.com>
2020-10-09 14:08:21 -0400

    Updates how underlying content is handled when overlays are fullscreen vs floating.

--- build/assets/js/custom/toggleDisplay.js
+++ build/assets/js/custom/toggleDisplay.js
@@ -13,26 +13,32 @@ const toggleDisplay = function(){
     if (targetEl.classList.contains('js__toggleDisplay--none')) {
       referrerPosY = window.scrollY;
       window.localStorage.setItem('referrerPosY', referrerPosY);
+
       if ( targetEl.getAttribute('data-fullscreenoverlay') === 'true' ) {
         body.classList.add('js__overlayBody--noScroll');
       }
-      targetEl.classList.remove('js__toggleDisplay--none');
-      // make sure you can see it, after its loaded
-      setTimeout(function() {
-        targetEl.scrollIntoView({behavior: "smooth"});
 
-        // if that doesn't work?
-        let pos = getElemDistance(targetEl);
-        if ((window.scrollY < pos.y) || (window.scrollY > pos.y)) {
-          window.scrollTo(0, pos.y - 100);
-        }
-      }, 250);
-    } else {
+      targetEl.classList.remove('js__toggleDisplay--none');
+
+      // if its not full screen, make sure you can see it, after its loaded
+      if ( targetEl.getAttribute('data-fullscreenoverlay') !== 'true' ) {
+        setTimeout(function() {
+          targetEl.scrollIntoView({behavior: "smooth"});
+
+          // if that doesn't work?
+          let pos = getElemDistance(targetEl);
+          if ((window.scrollY < pos.y) || (window.scrollY > pos.y)) {
+            window.scrollTo(0, pos.y - 100);
+          }
+        }, 250);
+      }
+    } else { // close the overlay
       referrerPosY = window.localStorage.getItem('referrerPosY');
       targetEl.classList.add('js__toggleDisplay--none');
       if ( body.classList.contains( 'js__overlayBody--noScroll' ) ) {
         body.classList.remove('js__overlayBody--noScroll');
       }
+      // put the content back where we were
       window.scrollTo( 0, referrerPosY);
     }
   }
